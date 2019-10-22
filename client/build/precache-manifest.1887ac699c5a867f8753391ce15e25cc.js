9822b54e8123596e43db8145fc1f64b033430ced
Pabbishetty <ajay.pabbishetty@capgemini.com>
2019-10-22 12:36:01 +0200

    new

---
+++ client/build/precache-manifest.1887ac699c5a867f8753391ce15e25cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8582c3b93c1e0693f3bb8ea6b86561cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b9035864231e3ad732e",
+    "url": "/static/css/2.18c71e96.chunk.css"
+  },
+  {
+    "revision": "99fdcc8399059fb3ed9c",
+    "url": "/static/css/main.e95c3c51.chunk.css"
+  },
+  {
+    "revision": "4b9035864231e3ad732e",
+    "url": "/static/js/2.58d9b309.chunk.js"
+  },
+  {
+    "revision": "99fdcc8399059fb3ed9c",
+    "url": "/static/js/main.8ee082b8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
